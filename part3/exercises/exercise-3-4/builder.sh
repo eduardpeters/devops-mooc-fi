13d7b5aa17ee6301dd6edf1c343c373efdf4b405
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-05-21 21:30:11 +0200

    Solve exercise 3 4

---
+++ part3/exercises/exercise-3-4/builder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 GIT_REPOSITORY DOCKERHUB_REPOSITORY" >&2
+  exit 1
+fi
+
+# Login to Docker
+docker login -u $DOCKER_USER -p $DOCKER_PWD
+
+# Use a temp directory to store repo
+TEMP="temp"
+GIT_URL="https://github.com/$1"
+DOCKER_REPOSITORY=$2
+echo $GIT_URL
+git clone $GIT_URL $TEMP
+
+# Build the docker image
+cd $TEMP
+docker build -t $DOCKER_REPOSITORY .
+cd -
+
+# Push the docker image
+docker push $DOCKER_REPOSITORY
